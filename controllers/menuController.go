d53aeb4fa5b55a2f460ea323bfa176fad605e9bf
ASPCODES <adityawork0811@gmail.com>
2026-08-23 16:12:07 +0530

    Completed the controller structure

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,1 +1,42 @@
 package controllers
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+
+func GetMenus() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+
+
+func GetMenu() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+
+func CreateMenu() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+
+func inTimeSpan(start, end, check time.Time) bool {
+	return start.After(time.Now()) && end.After(start)
+}
+
+
+func UpdateMenu() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,51 @@
 package controllers
+
+import(
+	"context"
+	"fmt"
+	"log"
+	"time"
+	"restio/database"
+	"restio/models"
+	helper "restio/helpers"
+
+	"github.com/gin-gonic/gin"
+
+)
+
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	}
+}
+
+func GetUser() gin.HandlerFunc  {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+
+func SignUp() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+
+func Login() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+
+func HashPassword(password string) string {
+
+}
+
+
+func VerifyPassword(userPassword string, providedPassword string) (bool, string)  {
+	
+}
