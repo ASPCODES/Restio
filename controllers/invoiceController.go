1323c4a793efc5a5852a505822db6dfe1a68eb76
ASPCODES <adityawork0811@gmail.com>
2026-09-05 00:31:04 +0530

    Implement GetInvoice function with error handling and response formatting

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"restio/database"
 	"restio/models"
@@ -55,7 +54,53 @@ func GetInvoices() gin.HandlerFunc {
 
 func GetInvoice() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		invoiceId := c.Param("invoice_id")
+		var invoice models.Invoice
+
+		err := invoiceCollection.FindOne(ctx, bson.M{"invoice_id": invoiceId}).Decode(&invoice)
+
+		if err == mongo.ErrNoDocuments {
+			c.JSON(http.StatusNotFound, gin.H{"error": "invoice not found"})
+			return
+		}
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while fetching invoice item"})
+			return 
+		}
+
+		allOrderItems, err := ItemsByOrder(invoice.Order_id)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while fetching order items"})
+			return 
+		}
+
+		if len(allOrderItems) == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "no order items found for this invoice"})
+			return 
+		}
+
+		var invoiceView InvoiceViewFormat
+		invoiceView.Order_id = invoice.Order_id
+		invoiceView.Payment_due_date = invoice.Payment_due_date
+		invoiceView.Invoice_id = invoice.Invoice_id
+		invoiceView.Payment_status = invoice.Payment_status
+
+		invoiceView.Payment_method = "null"
+
+		if invoice.Payment_method != nil {
+			invoiceView.Payment_method = *invoice.Payment_method
+		}
+
+		invoiceView.Payment_due = allOrderItems[0]["payment_due"]
+		invoiceView.Table_number = allOrderItems[0]["table_number"]
+		invoiceView.Order_details = allOrderItems[0]["order_items"]
+
+		c.JSON(http.StatusOK, invoiceView)
 	}
 }
 
