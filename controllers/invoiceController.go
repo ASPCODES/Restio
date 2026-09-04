3aa187dff622fcb8aef29efb01ef82d6962f4611
ASPCODES <adityawork0811@gmail.com>
2026-09-04 23:24:52 +0530

    Implement GetInvoices function to retrieve and return all invoice items

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,14 +1,16 @@
 package controllers
 
-
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"restio/database"
 	"restio/models"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type InvoiceViewFormat struct {
@@ -20,12 +22,33 @@ type InvoiceViewFormat struct {
 	Table_number		interface{}
 	Payment_due_date	time.Time
 	Order_details		interface{}
-
 }
+
+var invoiceCollection *mongo.Collection = database.OpenCollection(database.Client, "invoice")
 
 func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		
+		ctx, cancel := context.WithTimeout(context.Background(), 100* time.Second)
+		defer cancel()
+
+		result, err := invoiceCollection.Find(ctx, bson.M{})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing invoice items"})
+			return 
+		}
+
+		var allInvoices []bson.M
+		if err = result.All(ctx, &allInvoices); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while decoding invoice items"})
+			return 
+		}
+
+		if allInvoices == nil {
+			allInvoices = []bson.M{}
+		}
+
+		c.JSON(http.StatusOK, allInvoices)
 	}
 }
 
