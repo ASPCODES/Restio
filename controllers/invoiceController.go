1b687d2eeb496fe7eaa2a9ef2228d21ab6908914
ASPCODES <adityawork0811@gmail.com>
2026-09-04 23:02:27 +0530

    Add InvoiceViewFormat struct to invoiceController for invoice data representation

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -2,9 +2,26 @@ package controllers
 
 
 import (
+	"context"
+	"fmt"
+	"restio/database"
+	"restio/models"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+type InvoiceViewFormat struct {
+	Invoice_id 			string
+	Payment_method      string
+	Order_id			string
+	Payment_status		*string
+	Payment_due			interface{}
+	Table_number		interface{}
+	Payment_due_date	time.Time
+	Order_details		interface{}
+
+}
 
 func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {
